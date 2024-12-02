60e593803b1fa22bcc58c938887065170127410a
V Fleuret <p-valentin.fleuret@devialet.com>
2024-12-02 14:11:14 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+
+// http://tiptopboards.free.fr/arduino_forum/viewtopic.php?f=2&t=102
+//https://www.sonelec-musique.com/electronique_realisations_preampli_micro_016.html
+
+
+
+#include <Arduino.h>
+
+// include SPI, MP3 and SD libraries
+#include <SPI.h>
+#include "../lib/Adafruit_VS1053.h"
+#include <SD.h>
+
+// define the pins used
+#define RESET 9      // VS1053 reset pin (output)
+#define CS 10        // VS1053 chip select pin (output)
+#define DCS 8        // VS1053 Data/command select pin (output)
+#define CARDCS A0     // Card chip select pin
+#define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
+
+#define REC_BUTTON 7
+
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(RESET, CS, DCS, DREQ, CARDCS);
+
+File recording;  // the file we will save our recording to
+#define RECBUFFSIZE 128  // 64 or 128 bytes.
+uint8_t recording_buffer[RECBUFFSIZE];
+
+// put function declarations here:
+
+
+void setup() {
+Serial.begin(11520);
+  Serial.println("Adafruit VS1053 Ogg Recording Test");
+
+  // initialise the music player
+  if (!musicPlayer.begin()) {
+    Serial.println("VS1053 not found");
+    while (1);  // don't do anything more
+  }
+
+  musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
+ 
+  if (!SD.begin(CARDCS)) {
+    Serial.println("SD failed, or not present");
+    while (1);  // don't do anything more
+  }
+  Serial.println("SD OK!");
+  
+  // Set volume for left, right channels. lower numbers == louder volume!
+  musicPlayer.setVolume(10,10);
+  
+  // when the button is pressed, record!
+  pinMode(REC_BUTTON, INPUT);
+  digitalWrite(REC_BUTTON, HIGH);
+  
+  // load plugin from SD card! We'll use mono 44.1KHz, high quality
+  if (! musicPlayer.prepareRecordOgg("v44k1q05.img")) {
+     Serial.println("Couldn't load plugin!");
+     while (1);    
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
